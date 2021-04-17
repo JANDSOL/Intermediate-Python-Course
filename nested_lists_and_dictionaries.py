b07cb1832de69e2fd5d15aa66d11c51085b24131
Juan Andrade <jpas.juan23@gmail.com>
2021-04-16 23:10:35 -0500

    Nested list in the nested_lists_and_dictionaries.py

--- nested_lists_and_dictionaries.py
+++ nested_lists_and_dictionaries.py
@@ -2,6 +2,13 @@ def run():
     my_list = [1, 'Hello', True, 4.5]
     my_dict = {'firstname': 'Juan', 'lastname' : 'Andrade'}
 
+    nested_list = [
+        {'firstname': 'Juan', 'lastname' : 'Andrade'},
+        {'years' : '19', 'actual state' : 'Single'},
+        {'favorite food': 'Seafood', 'favorite drink' : 'Coca-Cola'},
+        {"pet's name": 'kimba', 'favorite color': 'cold colors', },
+    ]
+
 
 if __name__ == '__main__':
     run()

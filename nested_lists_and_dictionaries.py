00859e2fa2b70da18b413ddbb1f27f328250eece
Juan Andrade <jpas.juan23@gmail.com>
2021-04-16 23:54:41 -0500

    Commented code in the nested_lists_and_dictionaries.py

--- nested_lists_and_dictionaries.py
+++ nested_lists_and_dictionaries.py
@@ -1,7 +1,9 @@
 def run():
+    """Common lists and dictionaries."""
     my_list = [1, 'Hello', True, 4.5]
     my_dict = {'firstname': 'Juan', 'lastname' : 'Andrade'}
 
+    """Nested list."""
     nested_list = [
         {'firstname': 'Juan', 'lastname' : 'Andrade'},
         {'years' : '19', 'actual state' : 'Single'},
@@ -9,18 +11,21 @@ def run():
         {"pet's name": 'kimba', 'favorite color': 'cold colors', },
     ]
 
+    """Nested dictionary."""
     nested_dict = {
         'fruits': ['apple, grape, banana, pear'],
         'programming languages': ['python', 'java', 'c++', 'kotlin'],
         'names': ['Jhon', 'Peter', 'Kike'],
     }
 
+    """Tour of the elements contained in the nested list."""
     for idx in nested_list:
         for key, value in idx.items():
             print(key + ':', value)
     
     print('')
 
+    """Tour of the elements contained in the nested dictionary."""
     for key, value in nested_dict.items():
             print(key + ':', value)
 

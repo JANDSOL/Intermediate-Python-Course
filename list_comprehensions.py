7e07685e5a63ea9d605496e6a2429bf3583d009b
Juan Andrade <jpas.juan23@gmail.com>
2021-04-18 23:52:49 -0500

    Structure change in common list in list_comprehension.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -12,17 +12,35 @@ def run():
     print('\n***List Comprehension***')
     print(squ_with_com)
 
-    """Challenge"""
-    my_list, multiple4, multiple6, multiple9 = [], [], [], []
-    for number in range(1, 2510):
-        multiple4.append(4 * number)
-        multiple6.append(6 * number)
-        multiple9.append(9 * number)
-    for i in multiple4:
-        if i in multiple6 and i in multiple9:
-            my_list.append(i)
+    """Common List Challenge"""
+    # my_list, multiple4, multiple6, multiple9 = [], [], [], []
+    my_list = []
+    multiple4, multiple6, multiple9 = 0, 0, 0
+    NUM_RANGE = range(1, 2510)
+    for multiple4 in NUM_RANGE:
+        multiple4 = multiple4 * 4
+        for multiple6 in NUM_RANGE:
+            multiple6 = multiple6 * 6
+            if multiple4 == multiple6:
+                for multiple9 in NUM_RANGE:
+                    multiple9 = multiple9 * 9
+                    if multiple4 == multiple9:
+                        my_list.append(multiple4)
+        # multiple4.append(4 * number)
+        # multiple6.append(6 * number)
+        # multiple9.append(9 * number)
+    # for i in multiple4:
+    #     if i in multiple6 and i in multiple9:
+    #         my_list.append(i)
     print('\n***Lista común del reto***')
     print(my_list)
+
+    # """List Comprehension Challenge"""
+    # multiple4, multiple6, multiple9 = \
+    # [4 * number and 6 * number and 9 * number for number in range(1,101)]
+    # my_list2 = [i for i  in multiple4 if i in multiple6 and i in multiple9]
+    # print('\n***List comprehension del reto***')
+    # print(my_list2)
 
 
 if __name__ == '__main__':

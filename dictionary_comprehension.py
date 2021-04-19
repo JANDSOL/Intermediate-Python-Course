ce49c8e292980237b64e20a432ea350f2aa9edd5
Juan Andrade <jpas.juan23@gmail.com>
2021-04-19 12:16:48 -0500

    Dictionary Comprehension in dictionary_comprehension.py

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -7,6 +7,11 @@ def run():
     print('***Lista Común***')
     print(my_dict)
 
+    """Dictionary Comprehension"""
+    my_dict_comp = {num: num**3 for num in range(1, 101) if num % 3 != 0}
+    print('\n***Dictionary Comprehension***')
+    print(my_dict_comp)
+
 
 if __name__ == '__main__':
     run()

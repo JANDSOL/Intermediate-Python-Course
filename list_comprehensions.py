870af0c5e0b3dac7ec5a7fd5db15d7886314739a
Juan Andrade <jpas.juan23@gmail.com>
2021-04-18 22:28:57 -0500

    Show lists, common and comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -4,10 +4,13 @@ def run():
     for number in range(1, 101):
         if number % 3 != 0:
             squares.append(number**2)
+    print('***Lista Común***')
+    print(squares)
 
-    """List comprehensions"""
+    """List comprehension"""
     squ_with_com = [number**2 for number in range(1, 101) if number % 3 != 0]
-
+    print('\n***List Comprehension***')
+    print(squ_with_com)
 
 if __name__ == '__main__':
     run()

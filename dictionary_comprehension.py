1a94e79aac1f44631a8d6e134bc5584b937a2330
Juan Andrade <jpas.juan23@gmail.com>
2021-04-19 12:12:57 -0500

    Common dictionary in dictionary_comprehension.py

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -1,5 +1,11 @@
 def run():
-    pass
+    """Common Dictionary"""
+    my_dict = {}
+    for num in range(1, 101):
+        if num % 3 != 0:
+            my_dict[num] = num**3
+    print('***Lista Común***')
+    print(my_dict)
 
 
 if __name__ == '__main__':

5d71a9ccfdc326d868c532d0f6a916e63481926d
Juan Andrade <jpas.juan23@gmail.com>
2021-04-18 21:14:06 -0500

    List comprehensions in list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -5,6 +5,9 @@ def run():
         if number % 3 != 0:
             squares.append(number**2)
 
+    """List comprehensions"""
+    squ_with_com = [number**2 for number in range(1, 101) if number % 3 != 0]
+
 
 if __name__ == '__main__':
     run()

d4876004f74cbf33f0b2a5ef44b323aa2c9e7fd6
Juan Andrade <jpas.juan23@gmail.com>
2021-04-18 20:49:47 -0500

    Common list in list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,5 +1,9 @@
 def run():
-    pass
+    """Common list"""
+    squares = []
+    for number in range(1, 101):
+        if number % 3 != 0:
+            squares.append(number**2)
 
 
 if __name__ == '__main__':

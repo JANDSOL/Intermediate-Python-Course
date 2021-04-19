c3df71fa06fd552a309c74b9776d5f00f520fde5
Juan Andrade <jpas.juan23@gmail.com>
2021-04-19 12:38:14 -0500

    Common dictionary challenge in dictionary_comprehension.py

--- dictionary_comprehension.py
+++ dictionary_comprehension.py
@@ -1,3 +1,5 @@
+from math import sqrt as square_root
+
 def run():
     """Common Dictionary"""
     my_dict = {}
@@ -11,6 +13,15 @@ def run():
     my_dict_comp = {num: num**3 for num in range(1, 101) if num % 3 != 0}
     print('\n***Dictionary Comprehension***')
     print(my_dict_comp)
+
+    """Common Dictionary Challenge"""
+    my_dict2 = {}
+    # I use round to limit the number of decimal places in
+    # the root of the value.
+    for num in range(1, 1001):
+        my_dict2[num] = round(square_root(num), 3)
+    print('\n***Diccionario Común del Reto***')
+    print(my_dict2)
 
 
 if __name__ == '__main__':

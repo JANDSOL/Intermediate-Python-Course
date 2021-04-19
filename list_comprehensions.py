010da102a703463d31129d412994c4fe409711e3
Juan Andrade <jpas.juan23@gmail.com>
2021-04-18 23:26:50 -0500

    Common list challenge in list_comprehensions.py

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -12,5 +12,18 @@ def run():
     print('\n***List Comprehension***')
     print(squ_with_com)
 
+    """Challenge"""
+    my_list, multiple4, multiple6, multiple9 = [], [], [], []
+    for number in range(1, 2510):
+        multiple4.append(4 * number)
+        multiple6.append(6 * number)
+        multiple9.append(9 * number)
+    for i in multiple4:
+        if i in multiple6 and i in multiple9:
+            my_list.append(i)
+    print('\n***Lista común del reto***')
+    print(my_list)
+
+
 if __name__ == '__main__':
     run()

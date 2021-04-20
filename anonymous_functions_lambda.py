bedb64eef908ae40a115e2c2a1382b63fcbf28e4
Juan Andrade <jpas.juan23@gmail.com>
2021-04-20 12:07:51 -0500

    One word palidrome without lambda

--- anonymous_functions_lambda.py
+++ anonymous_functions_lambda.py
@@ -1,6 +1,10 @@
-def run():
-    pass
-
+def palidrome(word):
+    """One word palidrome without lambda"""
+    if word == word[::-1]:
+        return True
+    else:
+        return False
+    
 
 if __name__ == '__main__':
-    run()
+    print(palidrome('eana'))

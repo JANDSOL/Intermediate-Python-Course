a167505b061333543bd286e60fd48157cc92eec9
Juan Andrade <jpas.juan23@gmail.com>
2021-04-20 12:22:56 -0500

    One word palindrome with lambda

--- anonymous_functions_lambda.py
+++ anonymous_functions_lambda.py
@@ -4,7 +4,14 @@ def palidrome(word):
         return True
     else:
         return False
-    
+
+
+def run():
+    """One word palindrome with lambda"""
+    palindrome_lambda = lambda word: word == word[::-1]
+    print('\nPalabra palíndroma con lambda:', palindrome_lambda('arenera'))
+
 
 if __name__ == '__main__':
-    print(palidrome('eana'))
+    print('Palabra palíndroma sin lambda:', palidrome('ana'))
+    run()

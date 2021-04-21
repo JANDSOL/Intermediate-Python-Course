8b455bd012b524719bed12ed0b15f8c3ab1cbe13
Juan Andrade <jpas.juan23@gmail.com>
2021-04-21 11:24:01 -0500

    Example without filter in hod_filter.py

--- hof_filter.py
+++ hof_filter.py
@@ -1,6 +1,9 @@
 def run():
-    """High Order Functions, Filter"""
-    pass
+    """Example Without Filter"""
+    my_list = [1, 4, 5, 6, 9, 13, 19, 21]
+    odd_nums = [i for i in my_list if i % 2 != 0]
+    print('***Números impares con List Comprehension***')
+    print(odd_nums)
 
 
 if __name__ == '__main__':

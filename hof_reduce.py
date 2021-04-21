f7b1ab42175f6d2ae07f2f09e4fc24d630e20e49
Juan Andrade <jpas.juan23@gmail.com>
2021-04-21 13:26:39 -0500

    Example with reduce in hof_reduce.py

--- hof_reduce.py
+++ hof_reduce.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 def run():
     """Example Without Reduce"""
     my_list = [2, 2, 2, 2, 2]
@@ -6,6 +8,10 @@ def run():
     for number in my_list:
         all_multiply = all_multiply * number
     print('Ejemplo sin reduce:', all_multiply)
+
+    """Example With Reduce"""
+    all_multiply2 = reduce(lambda a, b: a * b, my_list)
+    print('\nEjemplo con reduce:', all_multiply2)
 
 
 if __name__ == '__main__':

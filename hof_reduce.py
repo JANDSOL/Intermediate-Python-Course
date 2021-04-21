c55f59d12740e48f8628d1a6f046684135937dc4
Juan Andrade <jpas.juan23@gmail.com>
2021-04-21 13:14:28 -0500

    Example without reduce in hof_reduce.py

--- hof_reduce.py
+++ hof_reduce.py
@@ -1,5 +1,11 @@
 def run():
-    pass
+    """Example Without Reduce"""
+    my_list = [2, 2, 2, 2, 2]
+    all_multiply = 1
+
+    for number in my_list:
+        all_multiply = all_multiply * number
+    print('Ejemplo sin reduce:', all_multiply)
 
 
 if __name__ == '__main__':

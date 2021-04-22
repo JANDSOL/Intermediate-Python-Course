1d6a29f3e73737d5fcbb97e3e22333bcae7b1a85
Juan Andrade <jpas.juan23@gmail.com>
2021-04-21 22:18:35 -0500

    High order functions workers adults in project_filtering_data.py

--- project_filtering_data.py
+++ project_filtering_data.py
@@ -84,6 +84,14 @@ def run():
     all_platzi_workers = [worker['name'] for worker in DATA if worker['organization'] == 'Platzi']
     for worker in all_platzi_workers:
         print('Platzi workers:', worker)
+    print('')
+    
+    """High Order Functions"""
+    # Contains the workers adults.
+    adults = list(filter(lambda worker: worker['age'] > 18, DATA))
+    adults = list(map(lambda worker: worker['name'], adults))
+    for worker in all_platzi_workers:
+        print('Workers adults:', worker)
 
 
 if __name__ == '__main__':

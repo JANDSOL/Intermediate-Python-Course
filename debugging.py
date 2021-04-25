dcc98e04bcebe68aae57fbbd096e0c7796596bf6
Juan Andrade <jpas.juan23@gmail.com>
2021-04-25 17:49:59 -0500

    Receive a number as a parameter and return a list with all its divisors in debugging.py

--- debugging.py
+++ debugging.py
@@ -1,5 +1,13 @@
+def divisors(num):
+    """Receive a number as a parameter and
+    return a list with all its divisors"""
+    divisors = [i for i in range(1, num + 1) if num % i == 1]
+    return divisors
+
 def run():
-    pass
+    num = int(input('Ingresa un número: '))
+    print('\nDIVISORES DE', num)
+    print(divisors(num))
 
 
 if __name__ == '__main__':

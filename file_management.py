9037b0f3ea7a7b5969893d272fc072a110c2d195
Juan Andrade <jpas.juan23@gmail.com>
2021-04-26 11:40:38 -0500

    Read file and convert to lists COMPLETE in file_management.py

--- file_management.py
+++ file_management.py
@@ -1,14 +1,15 @@
 def read():
     """Read files containing the numbers 1-10
     and convert each of their lines into lists"""
-    pass
+    with open("./archives/numbers.txt", 'r', encoding='UTF-8') as f:
+        numbers = [int(line) for line in f]
+        print(numbers)
 
 def write():
-
     pass
 
 def run():
-    pass
+    read()
 
 
 if __name__ == '__main__':

56376a11c14560252eb9eb5ee91680268291c808
Juan Andrade <jpas.juan23@gmail.com>
2021-04-25 20:18:16 -0500

    Palindrome, try-except and rise in exceptions_handling.py

--- exceptions_handling.py
+++ exceptions_handling.py
@@ -1,10 +1,17 @@
 def palindrome(string):
-    """Try and except"""
-    return string == string[::-1]
+    """Try, except and rise"""
+    try:
+        if len(string) == 0:
+            raise ValueError('No se puede ingresar una cadena vacía.')
+        return string == string[::-1]
+    except ValueError as ve:
+        print(ve)
+        return False
 
 
 if __name__ == '__main__':
     try:
-        print(palindrome(1))
+        print(palindrome(''))
+        print('Fin del programa.')
     except TypeError:
         print('Solo puede ingresar strings.')

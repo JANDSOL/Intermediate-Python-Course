e82a59ff25863b34772c84deead5441a514d9ca4
Juan Andrade <jpas.juan23@gmail.com>
2021-04-25 23:12:12 -0500

    Receive a number as a parameter and return a list with all its divisors in assert_statements.py

--- assert_statements.py
+++ assert_statements.py
@@ -1,5 +1,13 @@
+def divisors(num):
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
+    return divisors
+
 def run():
-    pass
+    """Receive a number as a parameter and
+    return a list with all its divisors"""
+    num = int(input('Ingresa un número: '))
+    print('\nDIVISORES DE', num)
+    print(divisors(num))
 
 
 if __name__ == '__main__':

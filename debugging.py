7a7345ab0c0c3ea50ef68d2c11e339b1218569e9
Juan Andrade <jpas.juan23@gmail.com>
2021-04-25 19:21:48 -0500

    Debug in list comprehension divisors, in debugging.py

--- debugging.py
+++ debugging.py
@@ -1,7 +1,7 @@
 def divisors(num):
     """Receive a number as a parameter and
     return a list with all its divisors"""
-    divisors = [i for i in range(1, num + 1) if num % i == 1]
+    divisors = [i for i in range(1, num + 1) if num % i == 0]
     return divisors
 
 def run():

e26ca993718d3f0af3831c8a6b6acbbad99c5f59
Juan Andrade <jpas.juan23@gmail.com>
2021-04-26 11:45:10 -0500

    Save names from a list to a file in file_management.py

--- file_management.py
+++ file_management.py
@@ -1,15 +1,20 @@
 def read():
     """Read files containing the numbers 1-10
     and convert each of their lines into lists"""
-    with open("./archives/numbers.txt", 'r', encoding='UTF-8') as f:
+    with open("./archives/numbers.txt", 'r', encoding="UTF-8") as f:
         numbers = [int(line) for line in f]
         print(numbers)
 
 def write():
-    pass
+    """Save names from a list to a file"""
+    names = ['Juan', 'Pablo', 'Pedro', 'Jesús', 'María']
+    with open("./archives/names.txt", "w", encoding="UTF-8") as f:
+        for name in names:
+            f.write(name)
+            f.write('\n')
 
 def run():
-    read()
+    write()
 
 
 if __name__ == '__main__':

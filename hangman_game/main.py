1d656a1417e8ba42ff22c4acaa21102dc34be36e
Juan Andrade <jpas.juan23@gmail.com>
2021-04-27 10:43:58 -0500

    Fase 1, Print name of game

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -1,5 +1,19 @@
+from os import system as window_clear    # In charge of refreshing our screen
+from name_of_game import NameOfGame
+
+
+def name_game():
+    """It will print the name of the game"""
+    NAME_OF_GAME = NameOfGame().NAME_GAME
+    count = 0
+    while(count < 21):
+        for name in NAME_OF_GAME:
+            print(name)
+            window_clear('cls')
+        count += 1
+
 def main():
-    pass
+    name_game()
 
 
 if __name__ == '__main__':

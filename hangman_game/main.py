c4ca6ee2c55306ebbb27e8fd6f65a6b4fb8d855a
Juan Andrade <jpas.juan23@gmail.com>
2021-04-30 12:12:49 -0500

    Check if the letters were found in their entirety in main.py

--- hangman_game/main.py
+++ hangman_game/main.py
@@ -6,7 +6,7 @@ from levels import Levels
 from images import Images
 from game_over import GameOver
 from character_replacement import CharacterReplacement as \
-    dashboard
+                                  dashboard
 
 
 def name_game():
@@ -21,27 +21,34 @@ def name_game():
         count += 1
 
 def list_words():
-    """Contains the list with words to guess"""
+    """Contains the list with words to guess."""
     with open('./archive/data.txt', 'r', encoding='UTF-8') as f:
         ls_words = [word.replace('\n', '').replace(' ', '') for word in f]
+
         return ls_words
 
 def remove_accents(w_from_list, w_entered):
+    """Remove the accents to avoid mistakes with
+    the accents of the word to search."""
     w_from_list = w_from_list.lower().replace('á', 'a').replace('é', 'e')\
                    .replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
     w_entered = w_entered.lower().replace('á', 'a').replace('é', 'e')\
                    .replace('í', 'i').replace('ó', 'o').replace('ú', 'u')
+
     return w_from_list, w_entered
 
 def list_random():
     """It will bring up random words without
     repetitions from the word list."""
     words_already_chosen = []
-    # Restart words without repetitions if all words were covered.
+    # if len(words_already_chosen) - 1: Restart words
+    # without repetitions if all words were covered.
     if len(words_already_chosen) - 1 == NUM_OF_WORDS_IN_LIST:
         while(len(words_already_chosen) != 0):
             words_already_chosen.pop()
-    # Check that there are no words that are repeated in the game.
+
+    # while(True): Check that there are no words
+    # that are repeated in the game.
     while(True):
         idx_list_random = randrange(0, NUM_OF_WORDS_IN_LIST + 1)
         if idx_list_random not in words_already_chosen:
@@ -51,6 +58,7 @@ def list_random():
     return WORDS[idx_list_random]
 
 def ask_continue_playing(keep_playing):
+    """Ask the user if they want to continue playing."""
     k_playing = ''
     while (True):
         k_playing = input('\n¿QUIERES SEGUIR JUGANDO?\n')
@@ -73,6 +81,21 @@ def ask_continue_playing(keep_playing):
 
     return keep_playing
 
+def check_complete_letters(found_letters, word):
+    """Check if the letters were found in their entirety."""
+    level_complete = False
+    repetition_counter = 0
+    if len(found_letters) == len(word):
+        for letter_entered in found_letters.values():
+            if letter_entered in word:
+                repetition_counter += 1
+        
+    if repetition_counter == len(word):
+        level_complete = True
+    window_clear('cls')
+
+    return level_complete
+
 def main():
     """Game Body"""
     name_game()
@@ -81,6 +104,8 @@ def main():
 
     while (keep_playing == True):  # Loop to keep playing.
         level_complete = False
+        # lap_counter will contain the index regarding the state
+        # in which the doll was left if the user wins.
         lap_counter = 0
         word = list_random()
         found_letters = {}
@@ -89,25 +114,25 @@ def main():
         for num_image, picture_level in enumerate(LEVEL):  # Level tour.
 
             while(level_complete == False):  # Loop to stop at each level.
+
                 if num_image < 6:
-                    """Validate if invalid characters are entered"""
+                    # Validate if invalid characters are entered
                     print(picture_level)
                     print(DOLL_PICTURE[num_image])
                     dashboard(word, formatted_word, string_entered,formatted_string_ent, \
                               found_letters)
+                    print(word, found_letters)
                     string_entered = input('Ingresa una letra o palabra: ')
                     string_entered = string_entered.replace(' ', '')
-                    print(found_letters)
-                    if string_entered.isalpha():
+                    if string_entered.isalpha():  # Validate in case of entering an error character.
                         formatted_word, formatted_string_ent = \
                         remove_accents(word, string_entered)
-                        # Check if it's letter.
+                        # if len(string_entered) == 1: Check if it's letter.
                         if len(string_entered) == 1:
-                            if formatted_string_ent in formatted_word:  
-                                """TODO:
-                                        1. Condicional para que confirme si las letras
-                                           completaron la palabra a buscar"""
-                                window_clear('cls')
+                            if formatted_string_ent in formatted_word:
+                                dashboard(word, formatted_word, string_entered, \
+                                          formatted_string_ent, found_letters)
+                                level_complete = check_complete_letters(found_letters, word)
                             else:
                                 lap_counter += 1
                                 window_clear('cls')
